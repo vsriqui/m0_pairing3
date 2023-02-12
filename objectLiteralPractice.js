094e8ea3fef87786787c2f12e07ef68a45275df5
Victor Sriqui <victorsriqui@gmail.com>
2023-02-12 12:21:29 -0700

    Initial commit

---
+++ objectLiteralPractice.js
@@ -0,0 +1,20 @@
+var person = {
+    myName: "Alec",
+    age: 30,
+    height: "5ft 9in",
+    location: "Philadelphia"
+
+}
+
+console.log(person);
+
+console.log(person.age);
+
+person.gender = "male";
+
+person.likesCrabCakes = true;
+
+console.log(person);
+
+console.log(Object.keys(person));
+
